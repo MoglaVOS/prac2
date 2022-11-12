270a6b4b5d3bf75df21ca4a9792651c8255d32c8
MoglaVOS <yurinandrew.29@gmail.com>
2022-11-12 06:42:50 +0300

    update

--- main.cpp
+++ main.cpp
@@ -6,21 +6,22 @@
 using namespace std;
 using namespace chrono;
 
-const int N = 100, maxNum = 99, minNum = -99;
+const int N = 100;
 
-void coutarr(int arr[], int N)
-{
+void coutarr(int arr[], int N) {
     for (int i = 0; i < N; i++)
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
 }
-void bubblesort(int* arr, int N){
-    for(int i = 0; i < N - 1; i++)
-        for(int j = 0; j < N - i - 1; j++)
-            if (arr[j] > arr[j+1]){
-                swap(arr[j], arr[j+1]);
+
+void bubblesort(int *arr, int N) {
+    for (int i = 0; i < N - 1; i++)
+        for (int j = 0; j < N - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
             }
 }
-void shakersort(int* arr, int N){
+
+void shakersort(int *arr, int N) {
     for (int j = 0; j < N / 2; j++) {
         for (int i = 0; i < N - 1; i++) {
             if (arr[i] > arr[i + 1])
@@ -32,7 +33,8 @@ void shakersort(int* arr, int N){
         }
     }
 }
-void insertsort(int* arr, int N) {
+
+void insertsort(int *arr, int N) {
     for (int i = 1; i < N; i++) {
         for (int j = i - 1; j >= 0; j--)
             if (arr[i] < arr[j]) {
@@ -55,7 +57,7 @@ int binarysearch(int arr[], int x, int low, int high) {
     return -1;
 }
 
-int main(){
+int main() {
     setlocale(LC_ALL, "ru_RU");
     int arr[N] = {};
     int commander;
@@ -70,13 +72,12 @@ int main(){
             while (firststart) {
                 cout << "[ ";
                 for (int i = 0; i < N; i++) {
-                    arr[i] = minNum + rand() % (maxNum - minNum + 1);
+                    arr[i] = -99 + rand() % (199);
                     cout << arr[i] << " ";
                 }
                 cout << "]" << endl;
                 break;
             }
-            arr[100] = 99;
         }
         case 2: {
             // 2. Сортировка массива
@@ -125,14 +126,15 @@ int main(){
                         auto elapsed = duration_cast<nanoseconds>(end - start);
                         coutarr(insert, N);
                         cout << endl;
-                        cout << "Сортировка заняла " << elapsed.count() << " наносекунд\n";
+                        cout << "Сортировка заняла " << elapsed.count() << " наносекунд";
                         cout << endl;
                         break;
                     }
                     default:
                         cout << endl;
                 }
-                break;
+                cout << "\nВыбрать другую сортировку? да - 1; нет - 0" << endl;
+                cin >> cycleone;
             }
         }
         case 3: {
@@ -180,23 +182,20 @@ int main(){
             for (int i = 0; i < N; i++)
                 if (arr[i] == avg) {
                     k += 1;
-                    cout << "arr [" << i << "] = ";
+                    cout << "arr[" << i << "] = ";
                 }
             if (k != 0) cout << avg << " ";
             cout << "Количество равных значений: " << k << endl;
         }
         case 5: {
-            shakersort(arr,N);
+            shakersort(arr, N);
             int a, cnt(0);
-            cout<<"\nВведите число а:\n";
-            cin>>a;
-            for (int i = 0; i < N; i++)
-            {
-                if (arr[i]<a)
-                {
+            cout << "\nВведите число а:\n";
+            cin >> a;
+            for (int i = 0; i < N; i++) {
+                if (arr[i] < a) {
                     cnt++;
-                }
-                else
+                } else
                     break;
             }
             if (cnt > 0)
@@ -205,17 +204,14 @@ int main(){
                 cout << "\nНет таких элементов";
         }
         case 6: {
-            shakersort(arr,N);
+            shakersort(arr, N);
             int b, cnt(0);
             cout << "\nВведите число b:\n";
             cin >> b;
-            for (int i = N-1; i >= 0; i--)
-            {
-                if (arr[i]>b)
-                {
+            for (int i = N - 1; i >= 0; i--) {
+                if (arr[i] > b) {
                     cnt++;
-                }
-                else
+                } else
                     break;
             }
             if (cnt > 0)
@@ -223,35 +219,42 @@ int main(){
             else
                 cout << "\nНет таких элементов";
         }
-        case 7:{
-            int x;
-            cout << "\nВведите число в диапазоне [-99,99] \n";
-            cin >> x;
-            {
-                auto start = steady_clock::now();
-                int result = binarysearch(arr, x, 0, N - 1);
-                auto end1 = steady_clock::now();
-                auto elapsed = duration_cast<nanoseconds>(end1 - start);
-                if (result == -1)
-                    cout << "Число не найдено";
-                else
-                    cout << "Элемент найден по индексу: " << result;
+        case 7: {
+            cout << "\nБинарный поиск: 1 - запустить цикл; 0 - закончить цикл" << endl;
+            int cycleseven;
+            cin >> cycleseven;
+            while (cycleseven) {
+                cout << "\nВведите число в диапазоне [-99,99]\n";
+                int x;
+                cin >> x;
+                {
+                    auto start = steady_clock::now();
+                    int result = binarysearch(arr, x, 0, N - 1);
+                    auto end1 = steady_clock::now();
+                    auto elapsed = duration_cast<nanoseconds>(end1 - start);
+                    if (result == -1)
+                        cout << "Число не найдено";
+                    else
+                        cout << "Элемент найден по индексу: " << result;
+                    cout << endl;
+                    cout << "Бинарный поиск занял " << elapsed.count() << " наносекунд\n";
+                }
                 cout << endl;
-                cout << "Бинарный поиск занял " << elapsed.count() << " наносекунд\n";
+                //------------------------------------------------------------------------------------------------------
+                auto start = steady_clock::now();
+                for (int i = 0, k = 0; i < N; i++) {
+                    if (arr[i] == x) cout << "Введенное число находится в массиве с номером " << i << endl;
+                    k += 1;
+                    if (k == 0 && i == 99) cout << "Введенное число не входит в массив\n";
+                }
+                auto endTime = steady_clock::now();
+                auto elapsed = duration_cast<nanoseconds>(endTime - start);
+                cout << "Перечисление элементов заняло  " << elapsed.count() << " наносекунд\n";
+                cout << "\nПродолжить цикл? 1 - да; 2 - нет\n";
+                cin >> cycleseven;
             }
-            cout << endl;
-            // перебор
-            auto start = steady_clock::now();
-            for (int i = 0, k = 0; i < N; i++) {
-                if (arr[i] == x) cout << "Введенное число находится в массиве с номером " << i << endl;
-                k += 1;
-                if (k == 0 && i == 99) cout << "Введенное число не входит в массив\n";
-            }
-            auto endTime = steady_clock::now();
-            auto elapsed = duration_cast<nanoseconds>(endTime - start);
-            cout << "Перечисление элементов заняло  " << elapsed.count() << " наносекунд\n";
         }
-        case 8:{
+        case 8: {
             int a, b;
             cout << "\nВведите индексы элементов, которые вы хотите поменять: \n";
             cin >> a >> b;
